8bc5bf5b1f7db390dd511765798719b8710f0f58
John C. Lavelle <jlavelle604@gmail.com>
2020-11-02 19:45:29 -0500

    Bump stylish-haskell version

--- shell.nix
+++ shell.nix
@@ -8,11 +8,11 @@ github-utils.shellFor {
     cabal = "3.2.0.0";
     haskell-language-server = "0.5.1";
     hlint = "3.2.1";
+    stylish-haskell = "0.12.2.0";
   };
   buildInputs = with pkgs.haskellPackages;
     [ ghcid
       hpack
-      stylish-haskell
     ];
   exactDeps = true;
 }
